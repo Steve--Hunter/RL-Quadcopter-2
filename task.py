dba11c9da49fd775e6093a418ae82e0c2719f4c2
Steve--Hunter <steve@smtahunter.com>
2018-08-21 12:43:41 +1000

    Update with feedback

--- task.py
+++ task.py
@@ -22,7 +22,7 @@ class Task():
         self.action_low = 390
         self.action_high = 420
         self.action_range = self.action_high - self.action_low 
-        self.action_size = 1
+        self.action_size = 4
 
         # Goal - hover at z=10, x,y=0        
         self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
@@ -31,13 +31,14 @@ class Task():
         """Uses current pose of sim to return reward."""
         reward = 0
         
-        reward = reward + 2.-0.3*(abs(self.sim.pose[2] - self.target_pos[2])).sum()
+        #reward = reward + 1.-0.3*(abs(self.sim.pose[2] - self.target_pos[2])).sum()
+        reward = np.tanh(1 - 0.005*(abs(self.sim.pose[:3] - self.target_pos))).sum()
         
            
         # penalize too far from target position
-        distance = np.linalg.norm(self.target_pos[2] - self.sim.pose[2])
-        if (distance > 2):
-            reward = reward - min(1,1/(distance + 1)**2)
+        #distance = np.linalg.norm(self.target_pos[2] - self.sim.pose[2])
+        #if (distance > 2):
+        #    reward = reward - min(1,1/(distance + 1)**2)
         
 
         return reward 
@@ -49,10 +50,11 @@ class Task():
         pose_all = []
         for _ in range(self.action_repeat):
             done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
-            done = self.sim.next_timestep(rotor_speeds)
-            done = self.sim.next_timestep(rotor_speeds)    
+   
             reward += self.get_reward()
             
+            # update rotor speed to achieve target height
+            # scale to +- 0.1, for input to the network
             z_norm = (self.sim.pose[2] - 10)/3
             z_v_norm = (self.sim.v[2]) / 3
             rotor_norm = (rotor_speeds[0] - self.action_low) / self.action_range * 2 - 1
@@ -64,11 +66,13 @@ class Task():
         return next_state, reward, done
 
     def reset(self):
-        """Reset the sim to start a new episode."""
+        """Reset the sim to start a new episode.
+        Start each episode with the Quadcopyer a random distance (vertically) away from the target height
+        """
         self.sim.reset()
         
         #perturb the start by +- one unit
-        perturb_unit = 2
+        perturb_unit = 0.1
         self.sim.pose[2] += (2*np.random.random()-1) * perturb_unit
 
         z_norm = (self.sim.pose[2] - 10)/10
